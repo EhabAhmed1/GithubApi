959350e681bdf1d7e1da1fa36d658c5a44e40b00
Ayman Ahmed <ayman.ahmed.ebrahim95@gmail.com>
2023-08-22 00:41:19 +0300

    initial commit and add retrofit dependencies

---
+++ app/src/main/java/com/example/githubclient/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.githubclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/githubclient/Repo.java
@@ -0,0 +1,49 @@
+package com.example.githubclient;
+
+public class Repo {
+    private String id;
+    private String full_name;
+    private String name;
+    private String description;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
